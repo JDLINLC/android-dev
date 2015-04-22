43083dd64d02b06f07322d2f2e23d27ce461ee88
JDLIN <2837848465@qq.com>
2015-04-22 22:57:27 +0800

    add image

--- ModifyQQ/gen/com/example/modifyqq/R.java
+++ ModifyQQ/gen/com/example/modifyqq/R.java
@@ -12,6 +12,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int th=0x7f020001;
     }
     public static final class id {
         public static final int Login=0x7f060007;
@@ -19,13 +20,17 @@ public final class R {
         public static final int button_1=0x7f060003;
         public static final int edit1_text=0x7f060001;
         public static final int edit2_text=0x7f060002;
+        public static final int image_view=0x7f060000;
         public static final int name=0x7f060004;
         public static final int tel=0x7f060006;
-        public static final int text_view=0x7f060000;
+        public static final int title_back=0x7f060008;
+        public static final int title_edit=0x7f06000a;
+        public static final int title_text=0x7f060009;
     }
     public static final class layout {
         public static final int main_activity=0x7f030000;
         public static final int showdetail_activity=0x7f030001;
+        public static final int title=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
