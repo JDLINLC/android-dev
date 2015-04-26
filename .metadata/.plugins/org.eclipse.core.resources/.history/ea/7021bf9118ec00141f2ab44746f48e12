0b49ce1e3cb8692d2bf39b074e379e48f4e63e5d
JDLIN <2837848465@qq.com>
2015-04-26 21:33:26 +0800

    commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ea/7021bf9118ec00141f2ab44746f48e12
@@ -0,0 +1,15 @@
+package com.example.modifyqq;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MsgActivity extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.msg_activity);
+	}
+	
+
+}
--- ModifyQQ/src/com/example/modifyqq/MsgActivity.java
+++ ModifyQQ/src/com/example/modifyqq/MsgActivity.java
@@ -1,14 +1,51 @@
 package com.example.modifyqq;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MsgActivity extends Activity{
+	private EditText edit;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.msg_activity);
+		edit =(EditText)findViewById(R.id.input_message1);
+		//String inputText = load();
+		//if (!TextUtile.isEmpty)
+	}
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+		String inputText =edit.getText().toString();
+		save(inputText);
+	}
+	
+	public void save(String inputText){
+		FileOutputStream out = null;
+		BufferedWriter writer = null;
+		try{
+			out = openFileOutput("data",Context.MODE_PRIVATE);
+			writer = new BufferedWriter(new OutputStreamWriter(out));
+			writer.write(inputText);
+		}catch(IOException e){
+			e.printStackTrace();
+		}finally{
+			try{
+				if(writer !=null){
+					writer.close();
+				}
+			}catch(IOException e){
+				e.printStackTrace();
+			}
+		}
 	}
 	
 
