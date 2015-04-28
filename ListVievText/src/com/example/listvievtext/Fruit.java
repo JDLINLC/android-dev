c9512ff455efb06cd2752535d199dbb64db930ef
JDLIN <2837848465@qq.com>
2015-04-28 21:08:38 +0800

    Easy Use of ListView

---
+++ ListVievText/src/com/example/listvievtext/Fruit.java
@@ -0,0 +1,14 @@
+package com.example.listvievtext;
+
+public class Fruit {
+	private String name;
+	
+	public Fruit(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+}
