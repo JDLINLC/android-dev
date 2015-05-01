eacbe2276403c1196c86a8702338e3b47c5eb742
JDLIN <2837848465@qq.com>
2015-05-01 12:01:55 +0800

    add ActionBar and Tab

--- ListVievText/gen/com/example/listvievtext/R.java
+++ ListVievText/gen/com/example/listvievtext/R.java
@@ -47,10 +47,10 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
-        public static final int album=0x7f050004;
         public static final int app_name=0x7f050000;
-        public static final int artist=0x7f050003;
         public static final int hello_world=0x7f050001;
+        public static final int magazines=0x7f050004;
+        public static final int news=0x7f050003;
     }
     public static final class style {
         /** 
