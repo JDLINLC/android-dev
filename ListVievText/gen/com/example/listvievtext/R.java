312cf72d3da556694aacfa9c73187b3d5abbcade
JDLIN <2837848465@qq.com>
2015-05-01 17:15:24 +0800

    a big change

--- ListVievText/gen/com/example/listvievtext/R.java
+++ ListVievText/gen/com/example/listvievtext/R.java
@@ -22,17 +22,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int book=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int ic_launcher=0x7f020000;
+        public static final int id=0x7f020001;
         public static final int search=0x7f020002;
-        public static final int share=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f080004;
         public static final int fruit_name=0x7f080002;
         public static final int list_view=0x7f080000;
         public static final int search=0x7f080003;
-        public static final int share=0x7f080004;
         public static final int web_view=0x7f080001;
     }
     public static final class layout {
@@ -52,15 +50,6 @@ public final class R {
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -72,10 +61,9 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppBaseTheme=0x7f060003;
+        public static final int CustomActionBarTheme=0x7f060000;
+        public static final int MyActionBar=0x7f060001;
+        public static final int MyActionBarTitleText=0x7f060002;
     }
 }
