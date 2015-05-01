7fc1a7fd4eeccf03d1f43473c07e5831ed13a8d6
JDLIN <2837848465@qq.com>
2015-05-01 16:18:36 +0800

    a little change

--- ListVievText/gen/com/example/listvievtext/R.java
+++ ListVievText/gen/com/example/listvievtext/R.java
@@ -22,19 +22,17 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int delete=0x7f020000;
-        public static final int home=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int id=0x7f020003;
-        public static final int search=0x7f020004;
+        public static final int book=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int search=0x7f020002;
+        public static final int share=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int delete=0x7f080005;
+        public static final int action_settings=0x7f080005;
         public static final int fruit_name=0x7f080002;
-        public static final int home=0x7f080004;
         public static final int list_view=0x7f080000;
         public static final int search=0x7f080003;
+        public static final int share=0x7f080004;
         public static final int web_view=0x7f080001;
     }
     public static final class layout {
